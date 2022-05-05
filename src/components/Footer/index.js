fffde3211595adfc9fa97650360098652e5c8060
Elisa Vidal <eunice1vidal@yahoo.com>
2022-05-05 00:06:28 -0700

    updates

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about"></h1>
+      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <div className="my-2">
+        <p>
+         
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
