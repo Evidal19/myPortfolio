a7fbebaf6eaeccd7ae028fed6d697f6d4ab6b4af
Elisa Vidal <eunice1vidal@yahoo.com>
2022-05-05 16:56:51 -0700

    update

---
+++ src/components/Projects/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Header from '../Header/index.js';
+import Footer from '../Footer/index.js';
+
+function Project() {
+ 
+  return (
+    <div>
+     <Header></Header>
+     <Footer></Footer>         
+    </div>
+  );
+}
+
+export default Project;
